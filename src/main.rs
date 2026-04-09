2cb9a89a17075267a74aa490541c0f518a0f4c80
Isaac Yanez <isaac231467@gmail.com>
2026-04-08 20:39:14 -0500

    made a guessing game YAY

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io; // Similar to imports in JS
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("the secret number {secret_number}");
+
+
+    loop {
+       println!("Please input your guess.");
+       let mut guess = String::new();
+
+       io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+
+       let guess: u32 = guess.trim().parse() { // match expression
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+       println!("Your guessed: {guess}");
+       match guess.cmp(&secret_number) { // match expression
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+      }
+    }
 }
